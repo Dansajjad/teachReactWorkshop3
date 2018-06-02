41b16d2da4acf9d19641028004370920dd70bc8c
Dan Sajjad <dan.sajjad@gmail.com>
2018-06-02 10:26:17 -0700

    Clean up app.js - es6 destructuring

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,19 @@
-const greeting = React.createElement(
+
+/*
+	Cleanup - declarative style, describing which of the methods, 
+	I am using as part of the packages
+ */
+
+const { createElement } = React
+const { render } = ReactDOM
+
+const greeting = createElement(
 	'h1', 
 	{className: 'header', id: 'greeting', style: {backgroundColor:'red', fontSize:'80px'}},
 	'Hello World'
 )
 
-ReactDOM.render(
+render(
 	greeting, 
 	document.getElementById('react-container')
 )
