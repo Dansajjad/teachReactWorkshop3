b8372b3b7d1251a8b4bd5deddc402baa89c2911b
Dan Sajjad <dan.sajjad@gmail.com>
2018-06-04 00:49:38 -0700

    Pass data down using this.props

--- src/components/SkiDayCount.js
+++ src/components/SkiDayCount.js
@@ -5,9 +5,10 @@ const SkiDayCount = React.createClass({
 	render() {
 		return (
 			<div className='ski-day-count'>
-				<div className='total-days'><span>8 days</span></div>
-				<div className='powder-days'><span>3 days</span></div>
-				<div className='backcountry-days'><span>1 hiking day</span></div>
+				<div className='total-days'><span>{this.props.total}</span><span>days</span></div>
+				<div className='powder-days'><span>{this.props.powder}</span><span>days</span></div>
+				<div className='backcountry-days'><span>{this.props.backcountry}</span><span>days</span></div>
+				<div>{this.props.goal}</div>
 			</div>
 		)
 	}
