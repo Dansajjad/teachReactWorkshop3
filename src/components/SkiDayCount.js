08839223e25ff2104596f92545d3260e0b50bcea
Dan Sajjad <dan.sajjad@gmail.com>
2018-06-04 01:14:56 -0700

    Convert class component to stateless functional component and use ES6 destructuring for props

--- src/components/SkiDayCount.js
+++ src/components/SkiDayCount.js
@@ -1,27 +1,17 @@
 import React from 'react'
-import { Component } from 'react'
 import '../stylesheets/ui.scss'
 
-class SkiDayCount extends Component {
-	
-	percentToDecimal(decimal) {
-		return decimal * 100 + '%'
-	}
+const percentToDecimal = (decimal) =>  decimal * 100 + '%'
 
-	calcGoalProgress(total, goal) {
-		return this.percentToDecimal(total/goal) 
-	}
+const calcGoalProgress = (total, goal) => percentToDecimal(total/goal)
 
-	render() {
-		return (
-			<div className='ski-day-count'>
-				<div className='total-days'><span>{this.props.total}</span><span>days</span></div>
-				<div className='powder-days'><span>{this.props.powder}</span><span>days</span></div>
-				<div className='backcountry-days'><span>{this.props.backcountry}</span><span>days</span></div>
-				<div>{this.calcGoalProgress(this.props.total, this.props.goal)}</div>
-			</div>
-		)
-	}
-}
+const SkiDayCount = ({ total, powder, backcountry, goal }) => (
+		<div className='ski-day-count'>
+			<div className='total-days'><span>{total}</span><span>days</span></div>
+			<div className='powder-days'><span>{powder}</span><span>days</span></div>
+			<div className='backcountry-days'><span>{backcountry}</span><span>days</span></div>
+			<div>{calcGoalProgress(total, goal)}</div>
+		</div>
+)
 
 export default SkiDayCount
