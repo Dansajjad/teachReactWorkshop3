979cd1ebcab83d7c5de737476884f3c19ed4d681
Dan Sajjad <dan.sajjad@gmail.com>
2018-06-04 00:56:31 -0700

    Add methods to the component

--- src/components/SkiDayCount.js
+++ src/components/SkiDayCount.js
@@ -2,13 +2,22 @@ import React from 'react'
 import '../stylesheets/ui.scss'
 
 const SkiDayCount = React.createClass({
+	
+	percentToDecimal(decimal) {
+		return decimal * 100 + '%'
+	},
+
+	calcGoalProgress(total, goal) {
+		return this.percentToDecimal(total/goal) 
+	},
+
 	render() {
 		return (
 			<div className='ski-day-count'>
 				<div className='total-days'><span>{this.props.total}</span><span>days</span></div>
 				<div className='powder-days'><span>{this.props.powder}</span><span>days</span></div>
 				<div className='backcountry-days'><span>{this.props.backcountry}</span><span>days</span></div>
-				<div>{this.props.goal}</div>
+				<div>{this.calcGoalProgress(this.props.total, this.props.goal)}</div>
 			</div>
 		)
 	}
